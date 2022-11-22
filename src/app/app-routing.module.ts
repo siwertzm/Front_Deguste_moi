d235af43204762fb0ce43554ba21f770006eb475
jennygenie <jenny83than@gmail.com>
2022-11-22 17:07:19 +0100

    recettes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { RecettesComponent } from './recettes/recettes.component';
+
 
 const routes: Routes = [
   {component: ConnexionComponent, path: "login"},
-  {component: InscriptionComponent, path: "sign"}
+  {component: InscriptionComponent, path: "sign"},
+  {component: RecettesComponent, path:"recettes"}
 ];
 
 @NgModule({
