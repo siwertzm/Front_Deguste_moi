3bbcddad34b8b5306f4262c24b05b979f247589e
Augustin36000 <laaugustin@esic.fr>
2022-11-24 09:38:29 +0100

    OrgaRepas

---
+++ src/app/orga-repas/orga-repas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orga-repas',
+  templateUrl: './orga-repas.component.html',
+  styleUrls: ['./orga-repas.component.css']
+})
+export class OrgaRepasComponent {
+
+}
