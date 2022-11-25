a5151c4ae2f3e93a63d8da13095493221fd0da2a
siwertzm <siwertzm@gmail.com>
2022-11-25 16:35:16 +0100

    test

--- src/app/amis/amis.component.ts
+++ src/app/amis/amis.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-amis',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./amis.component.css']
 })
 export class AmisComponent {
+
+
+  user: any;
+  userConnected: any;
+  constructor(private http: HttpClient, private service: AuthService){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  rechercheByLogin(val: any) {
+    this.userConnected = this.service.getUserConnected();
+    this.http.get('http://localhost:8289/user/' + val).subscribe({
+      next: (data)=> { this.user = data; console.log(this.user); console.log(this.userConnected)},
+      error: (err)=> { console.log(err)}
+    });
+  }
 
 }
