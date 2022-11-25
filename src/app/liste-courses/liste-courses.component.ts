53009b5525a94e24469bc62b8f7a82e44c600bc4
jennygenie <jenny83than@gmail.com>
2022-11-25 11:12:17 +0100

    ajout liste courses

---
+++ src/app/liste-courses/liste-courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-courses',
+  templateUrl: './liste-courses.component.html',
+  styleUrls: ['./liste-courses.component.css']
+})
+export class ListeCoursesComponent {
+
+}
