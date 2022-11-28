6959715c777069864ef2601052c4d83181744c34
Romain <rspeck52@gmail.com>
2022-11-28 18:24:10 +0100

    ppp

--- src/app/amis/amis.component.ts
+++ src/app/amis/amis.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-
+import { Data, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
-import {Component, OnInit} from '@angular/core';
+import {Component, NgIterable, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
@@ -21,11 +21,14 @@ export class AmisComponent implements OnInit{
 
   user: any;
   userConnected: any;
-  constructor(private http: HttpClient, private service: AuthService){
+  demandeactu: any;
+
+  constructor(private http: HttpClient, private service: AuthService, private route : Router){
 
   }
 
   ngOnInit() {
+    this.gestionDemande();
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => {
@@ -46,18 +49,31 @@ export class AmisComponent implements OnInit{
 
   rechercheByLogin(val: any) {
     this.userConnected = this.service.getUserConnected();
-    this.http.get('http://localhost:8289/user/' + val).subscribe({
+    this.http.get('http://localhost:8289/user/'+ val).subscribe({
       next: (data)=> { this.user = data; console.log(this.user); console.log(this.userConnected)},
       error: (err)=> { console.log(err)}
     });
   }
 
 
-  demande(val:any){
-    this.http.post('http://localhost:8287/demande', val).subscribe({
+  demande(){
+   let demande = {envoyeur: this.userConnected, receveur: this.user, valide: false};
+  console.log(demande);
+    this.http.post('http://localhost:8289/demande', demande).subscribe({
       next: (data)=> {console.log(data)},
-      error: (err)=> {console.log(err); }
-  })
-  
+      error: (err)=> {console.log(err);}
+      
+  })}
+
+  gestionDemande(){
+    this.http.get('http://localhost:8289/demande').subscribe({
+    next: (data)=> {this.demandeactu = data; console.log(this.demandeactu)},
+    error: (err)=> {console.log(err)}
+    });
   }
+
+  accepterDemande(){
+    
+  }
+  
 }
