4c374295c5670ef6b7d6929a8365cda47ab6150e
siwertzm <siwertzm@gmail.com>
2022-11-28 10:36:22 +0100

    recette favoris

--- src/app/recettes-favs/recettes-favs.component.ts
+++ src/app/recettes-favs/recettes-favs.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { RecetteService } from '../services/recette.service';
 
 @Component({
   selector: 'app-recettes-favs',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./recettes-favs.component.css']
 })
 export class RecettesFavsComponent {
+
+  recette: any;
+  userConnected : any;
+  constructor(private http: HttpClient, private service: AuthService, private recetteService: RecetteService, private route: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.userConnected = this.service.getUserConnected();
+    this.recupeFavoris(this.userConnected.id);
+
+  }
+
+  recupeFavoris(val: any) {
+    this.http.get('http://localhost:8289/favori/user/' + val).subscribe({
+      next: (data) => { this.recette = data; console.log(this.recette); console.log(this.userConnected.id)},
+      error: (err) => { console.log(err); }
+
+    });
+
+    
+  }
+
+  gestionSubTitle(sub: string){
+    let res = sub.split("*", 10);
+    // console.log(res);
+    return res;
+  }
+
+  go(val: any) {
+    this.http.get('http://localhost:8289/recette/' + val).subscribe({
+      next: (data) => { this.recette = data; },
+      error: (err) => { console.log(err); }
+
+    });
+  }
+
+  goToRecette(val: any) {
+    this.recetteService.saveRecetteActu(val);
+    this.route.navigateByUrl('ficheRecette');
+
+  }
 
 }
