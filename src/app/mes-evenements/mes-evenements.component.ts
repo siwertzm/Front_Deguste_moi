e323bdc2801c2472828e5c668086d9cb186429d8
siwertzm <siwertzm@gmail.com>
2022-11-29 15:32:29 +0100

    invite

--- src/app/mes-evenements/mes-evenements.component.ts
+++ src/app/mes-evenements/mes-evenements.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-mes-evenements',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./mes-evenements.component.css']
 })
 export class MesEvenementsComponent {
+
+  invitation: any;
+  status: any;
+  constructor(private http: HttpClient, private authService: AuthService){
+
+  }
+
+  ngOnInit(){
+    this.recupeInvite(this.authService.getUserConnected().id);
+
+  }
+
+  recupeInvite(val: any){
+    this.http.get('http://localhost:8289/participation/accepted/' + val).subscribe({
+      next: (data)=> { this.invitation = data, console.log(this.invitation)},
+      error: (err)=> { console.log(err)}
+
+    });
+  }
+
+  supInvite(val: any){
+    console.log(val);
+    this.http.delete('http://localhost:8289/participation/reject/'+ val).subscribe(()=> this.status = 'delete success');
+    window.location.reload();
+    
+  }
+
+  acceptInvite(val: any, val2: any){
+    this.http.patch('http://localhost:8289/participation/accept/' + val, val2).subscribe(()=> this.status = 'accept');
+    window.location.reload();
+  }
 
 }
--- src/app/mes-invitations/mes-invitations.component.ts
+++ src/app/mes-invitations/mes-invitations.component.ts
@@ -16,13 +16,12 @@ export class MesInvitationsComponent {
   }
 
   ngOnInit(){
-    console.log(this.authService.getUserConnected().id)
     this.recupeInvite(this.authService.getUserConnected().id);
 
   }
 
   recupeInvite(val: any){
-    this.http.get('http://localhost:8289/participation/user/' + val).subscribe({
+    this.http.get('http://localhost:8289/participation/invite/' + val).subscribe({
       next: (data)=> { this.invitation = data, console.log(this.invitation)},
       error: (err)=> { console.log(err)}
 
@@ -30,10 +29,15 @@ export class MesInvitationsComponent {
   }
 
   supInvite(val: any){
-
-    this.http.delete('participation/reject/'+ val).subscribe(()=> this.status = 'delete success');
+    console.log(val);
+    this.http.delete('http://localhost:8289/participation/reject/'+ val).subscribe(()=> this.status = 'delete success');
     window.location.reload();
     
+  }
+
+  acceptInvite(val: any, val2: any){
+    this.http.patch('http://localhost:8289/participation/accept/' + val, val2).subscribe(()=> this.status = 'accept');
+    window.location.reload();
   }
 
 }
